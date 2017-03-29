c712dc3166371db092a4886a8b4a593386843442
Alexey Min <alexey.min@gmail.com>
2017-03-29 10:43:29 +0500

    Add cache class for evelink API cache, based on evelink's SqliteCache,
    but with fixed path in user's savedata directory

---
+++ core/em_cache.py
@@ -0,0 +1,14 @@
+import logging
+
+import core.logger
+import core.os_utils
+
+import evelink.cache.sqlite
+
+
+class EmCache(evelink.cache.sqlite.SqliteCache):
+    def __init__(self):
+        self._logger = core.logger.get_logger(__name__, logging.DEBUG)
+        path = core.os_utils.get_savedata_directory() + '/cache.db'
+        self._logger.debug('EmCache: using cache SQLite db file: {}'.format(path))
+        super(EmCache, self).__init__(path)
--- core/em_core.py
+++ core/em_core.py
@@ -4,6 +4,7 @@ import logging
 import core.logger
 import core.evedata
 import core.savedata
+import core.em_cache
 
 
 _g_emcore_instance = None
@@ -14,6 +15,7 @@ class EmCore:
         self.logger = core.logger.get_logger(__name__, logging.DEBUG)
         self.savedata = core.savedata.SaveData()
         self.evedata = core.evedata.EVEData()
+        self.cache = core.em_cache.EmCache()
 
 
 def get_core_instance() -> EmCore:
