5eac2a16bd5df37cdeca49db59ea2d7d71f43fae
Alexey Min <alexey.min@gmail.com>
2017-03-30 11:04:38 +0500

    EmCore: create evelink API object

--- core/em_core.py
+++ core/em_core.py
@@ -5,7 +5,10 @@ import core.logger
 import core.evedata
 import core.savedata
 import core.em_cache
+import core.models
+import version
 
+import evelink.api
 
 _g_emcore_instance = None
 
@@ -16,6 +19,16 @@ class EmCore:
         self.savedata = core.savedata.SaveData()
         self.evedata = core.evedata.EVEData()
         self.cache = core.em_cache.EmCache()
+        # create evelink API object
+        ver = version.get_pyevemon_version()
+        # "pyevemon/0.1 (alexey.min@gmail.com)"
+        self.user_agent_str = '{}/{} ({})'.format(
+            ver['app_name'], ver['version'], ver['author_email'])
+        self.api = evelink.api.API(cache=self.cache, user_agent=self.user_agent_str)
+
+    def set_apikey(self, apikey: core.models.EMApiKey):
+        # should be a tuple of (keyid, vcode)
+        self.api.api_key = (apikey.keyid, apikey.vcode)
 
 
 def get_core_instance() -> EmCore:
