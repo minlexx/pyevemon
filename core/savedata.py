72642577dd27617ce78599907cef34c1627b99e7
Alexey Min <alexey.min@gmail.com>
2017-04-10 14:12:13 +0500

    Savedata: store DB revision in db key/value table and in class. Run migrations automatically to upgrade database!

--- core/savedata.py
+++ core/savedata.py
@@ -7,10 +7,13 @@ import core.logger
 import sqlalchemy
 import sqlalchemy.orm
 
-import core.models
+from core.models import EmModelBase, EmApiKey, EmKeyValue
 
 
 class SaveData:
+
+    current_db_revision = 1
+
     def __init__(self):
         self._logger = core.logger.get_logger(__name__, logging.DEBUG)
         self.user_settings_file = core.os_utils.get_savedata_directory() + '/settings.db'
@@ -19,18 +22,45 @@ class SaveData:
         #
         # SQL Alchemy objects
         self.sql_engine = sqlalchemy.create_engine('sqlite:///'+self.user_settings_file, echo=False)
-        core.models.EmModelBase.metadata.create_all(self.sql_engine)
+        EmModelBase.metadata.create_all(self.sql_engine)
         # sqlalchemy session
         session_class = sqlalchemy.orm.sessionmaker(bind=self.sql_engine)
         self.sql_session = session_class()
+        # check database version for a possible upgrade
+        self._check_database()
+
+    def _check_database(self):
+        # get db version
+        dbrev = self._get_db_revision()
+        self._logger.debug('Got db revision: {}'.format(dbrev))
+        if dbrev < self.current_db_revision:
+            # run migration scripts?
+            while dbrev < self.current_db_revision:
+                self._run_migration(dbrev)
+                dbrev += 1
+        self._logger.debug('DB check complete')
+
+    def _get_db_revision(self) -> int:
+        res = self.sql_session.query(EmKeyValue).filter_by(key='_db_version').one_or_none()
+        if res is None:
+            return 0
+        rev = int(res.value)
+        return rev
+
+    def _run_migration(self, rev: int):
+        if rev == 0:
+            self._logger.debug('Running migration rev 0->1...')
+            with self.sql_engine.connect() as con:
+                con.execute('ALTER TABLE emapikey ADD COLUMN friendly_name TEXT')
+                con.execute("""INSERT OR REPLACE INTO emkeyvalue (key, value)
+                    VALUES ('_db_version', '1')""")
 
     def get_apikeys(self) -> list:
-        ret = self.sql_session.query(core.models.EmApiKey).all()
+        ret = self.sql_session.query(EmApiKey).all()
         return ret
 
-    def store_apikey(self, apikey: core.models.EmApiKey):
-        res = self.sql_session.query(core.models.EmApiKey).\
-            filter_by(keyid=apikey.keyid).one_or_none()
+    def store_apikey(self, apikey: EmApiKey):
+        res = self.sql_session.query(EmApiKey).filter_by(keyid=apikey.keyid).one_or_none()
         if res is None:
             self.sql_session.add(apikey)
             self.sql_session.commit()
