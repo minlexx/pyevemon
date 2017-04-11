096aff362bb1e7d8f4d2d962c30733c66e24de4d
Alexey Min <alexey.min@gmail.com>
2017-04-11 22:49:10 +0500

    Tests: be more professional!

--- tests/test_evedata.py
+++ tests/test_evedata.py
@@ -5,13 +5,17 @@ from core.evedata import EVEData
 
 class TestEveData(unittest.TestCase):
 
+    def setUp(self):
+        self.evedata = EVEData()
+
+    def tearDown(self):
+        del self.evedata
+
     def test_typeID(self):
-        ed = EVEData()
-        res = ed.find_typeid(3351)
+        res = self.evedata.find_typeid(3351)
         self.assertEqual(type(res), dict)
         self.assertEqual(res['groupID'], 258)
         self.assertEqual(res['typeName'], 'Shield Command Specialist')
-        pass
 
 
 if __name__ == '__main__':
