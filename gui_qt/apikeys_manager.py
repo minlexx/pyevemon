94744fb79e14ef24f6bc0097191abb389b82a5f8
Alexey Min <alexey.min@gmail.com>
2017-04-13 11:14:48 +0500

    Api keys manager: make it look nicer, allow selecting text

--- gui_qt/apikeys_manager.py
+++ gui_qt/apikeys_manager.py
@@ -2,9 +2,9 @@
 import logging
 
 from PyQt5.QtGui import QFont, QIcon, QCloseEvent
-from PyQt5.QtCore import pyqtSignal, pyqtSlot
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt
 from PyQt5.QtWidgets import QWidget, QLabel, QComboBox, QLineEdit, \
-    QPushButton, QVBoxLayout, QHBoxLayout, QPlainTextEdit, QMessageBox
+    QPushButton, QLayout, QVBoxLayout, QHBoxLayout, QPlainTextEdit, QMessageBox
 
 from core.logger import get_logger
 from core.em_core import get_core_instance
@@ -16,23 +16,41 @@ class SingleApiKeyWidget(QWidget):
         super(SingleApiKeyWidget, self).__init__(parent)
         #
         self._layout = QHBoxLayout()
+        self._layout.setSpacing(0)
         self.setLayout(self._layout)
         #
         self._lbl_keyname = QLabel('', self)
         font = self._lbl_keyname.font()
         font.setWeight(QFont.Bold)
         self._lbl_keyname.setFont(font)
+        self._lbl_keyname.setMinimumSize(100, 25)
+        self._lbl_keyname.setMaximumHeight(25)
         self._lbl_keyid = QLabel('', self)
+        self._lbl_keyid.setMinimumSize(100, 25)
+        self._lbl_keyid.setMaximumHeight(25)
+        self._lbl_keyid.setTextInteractionFlags(
+            Qt.TextSelectableByKeyboard | Qt.TextSelectableByMouse |
+            Qt.LinksAccessibleByKeyboard | Qt.LinksAccessibleByMouse)
         self._lbl_vcode = QLabel('', self)
+        self._lbl_vcode.setMinimumSize(100, 25)
+        self._lbl_vcode.setMaximumHeight(25)
+        self._lbl_vcode.setTextInteractionFlags(
+            Qt.TextSelectableByKeyboard | Qt.TextSelectableByMouse |
+            Qt.LinksAccessibleByKeyboard | Qt.LinksAccessibleByMouse)
         self._layout_v = QVBoxLayout()
         self._layout_v.addWidget(self._lbl_keyname)
         self._layout_v.addWidget(self._lbl_keyid)
         self._layout_v.addWidget(self._lbl_vcode)
+        self._layout_v.setSizeConstraint(QLayout.SetMinimumSize)
         self._layout.addLayout(self._layout_v)
         self._btn_edit = QPushButton(self.tr('Edit...'), self)
+        self._btn_edit.setMinimumHeight(25)
         self._btn_remove = QPushButton(self.tr('Remove'), self)
+        self._btn_remove.setMinimumHeight(25)
+        self._layout.addStretch()
         self._layout.addWidget(self._btn_edit)
         self._layout.addWidget(self._btn_remove)
+        self._layout.setSizeConstraint(QLayout.SetMinimumSize)
         #
         self.show()
 
@@ -74,8 +92,8 @@ class ApikeysManagerWindow(QWidget):
         # layouts
         self._layout_top1 = QHBoxLayout()
         self._layout_top1.addWidget(self._lbl_apikeys)
-        self._layout_top1.addWidget(self._btn_add_apikey)
         self._layout_top1.addStretch()
+        self._layout_top1.addWidget(self._btn_add_apikey)
 
         self._layout.addLayout(self._layout_top1, 0)
 
@@ -93,4 +111,5 @@ class ApikeysManagerWindow(QWidget):
         for apikey in apikeys:
             apikey_widget = SingleApiKeyWidget(self)
             apikey_widget.set_from_apikey(apikey)
-            self._layout.addWidget(apikey_widget)
+            self._layout.addWidget(apikey_widget, 1)
+        self._layout.setSizeConstraint(QLayout.SetMinimumSize)
