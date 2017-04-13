9fbedd9d23ff8124a90cf22b36703678aa374e66
Alexey Min <alexey.min@gmail.com>
2017-04-13 16:56:37 +0500

    Api keys manager: add stub signals/slots, connections

--- gui_qt/apikeys_manager.py
+++ gui_qt/apikeys_manager.py
@@ -12,8 +12,13 @@ from core.models import EmApiKey
 
 
 class SingleApiKeyWidget(QWidget):
+
+    editClicked = pyqtSignal('QString')
+    removeClicked = pyqtSignal('QString')
+
     def __init__(self, parent):
         super(SingleApiKeyWidget, self).__init__(parent)
+        self._logger = get_logger(__name__, logging.DEBUG)
         #
         self._layout = QHBoxLayout()
         self._layout.setSpacing(0)
@@ -46,19 +51,32 @@ class SingleApiKeyWidget(QWidget):
         self._layout.addLayout(self._layout_v)
         self._btn_edit = QPushButton(self.tr('Edit...'), self)
         self._btn_edit.setMinimumHeight(25)
+        self._btn_edit.clicked.connect(self.on_btn_clicked_edit)
         self._btn_remove = QPushButton(self.tr('Remove'), self)
         self._btn_remove.setMinimumHeight(25)
+        self._btn_remove.clicked.connect(self.on_btn_clicked_remove)
         self._layout.addStretch()
         self._layout.addWidget(self._btn_edit)
         self._layout.addWidget(self._btn_remove)
         self._layout.setSizeConstraint(QLayout.SetMinimumSize)
         #
         self.show()
+        #
+        self._apikey = None
 
     def set_from_apikey(self, apikey: EmApiKey):
+        self._apikey = apikey
         self._lbl_keyname.setText(self.tr('Name') + ': ' + apikey.friendly_name)
         self._lbl_keyid.setText('keyId: ' + apikey.keyid)
         self._lbl_vcode.setText('vCode: ' + apikey.vcode)
+
+    def on_btn_clicked_edit(self, checked: bool = False):
+        self._logger.debug('emit editClicked({})'.format(self._apikey.keyid))
+        self.editClicked.emit(self._apikey.keyid)
+
+    def on_btn_clicked_remove(self, checked: bool = False):
+        self._logger.debug('emit removeClicked({})'.format(self._apikey.keyid))
+        self.removeClicked.emit(self._apikey.keyid)
 
 
 class ApikeysManagerWindow(QWidget):
@@ -83,6 +101,7 @@ class ApikeysManagerWindow(QWidget):
 
         # buttons
         self._btn_add_apikey = QPushButton(self.tr('Add API key...'), self)
+        self._btn_add_apikey.clicked.connect(self.on_click_add_apikey)
 
         # layouts
         self._layout_top1 = QHBoxLayout()
@@ -107,5 +126,17 @@ class ApikeysManagerWindow(QWidget):
         for apikey in apikeys:
             apikey_widget = SingleApiKeyWidget(self)
             apikey_widget.set_from_apikey(apikey)
+            apikey_widget.editClicked.connect(self.on_click_edit_apikey)
+            apikey_widget.removeClicked.connect(self.on_click_remove_apikey)
             self._layout.addWidget(apikey_widget, 0)
+        #
         self._layout.addStretch()
+
+    def on_click_edit_apikey(self, keyid: str):
+        self._logger.debug('keyid = {}'.format(keyid))
+
+    def on_click_add_apikey(self):
+        self._logger.debug('click')
+
+    def on_click_remove_apikey(self, keyid: str):
+        self._logger.debug('keyid = {}'.format(keyid))
