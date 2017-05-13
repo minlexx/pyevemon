8890673cb84c8dff61d0d82b7bd6584b006e136f
Alexey Min <alexey.min@gmail.com>
2017-05-13 21:49:16 +0500

    Document logger module, add set_loglevel() function.
    It will control logging level of application (all loggers, created in the future)

--- core/logger.py
+++ core/logger.py
@@ -1,4 +1,30 @@
 # -*- coding: utf-8 -*-
+"""
+Core logger module.
+The whole purpose of this module is hiding the complexity of built-in logging module.
+All this log handlers, log formatters setup. Unhandled exceptions interception setup. All this.
+
+Usage of this module is simple. At the very beginning of program, you initialize it:
+
+    import logging
+    import core.logger
+
+    core.logger.init_unhandled_exception_handling('ungandled_exceptions.log')
+    core.logger.set_loglevel(logging.DEBUG)
+
+Then you create your logger:
+    
+    logger = core.logger.get_logger(__name__)
+
+And use it as standard logging.Logger object:
+
+    logger.debug('...')
+    logger.info('...')
+    logger.warn('...')
+    logger.error('...')
+    logger.exception('...', exc)   # from exception handler
+"""
+
 import logging
 import logging.handlers
 import sys
@@ -14,7 +40,23 @@ _g_evemon_unhandled_exception_handlers = []
 _g_evemon_unhandled_exception_params = {}
 
 
+def set_loglevel(level: int = logging.DEBUG):
+    """
+    Sets logging level of application. Affects only loggers created after this call
+    :param level: constant from logging.module, for example logging.DEBUG or logging.INFO
+    :return: None
+    """
+    global _g_evemon_default_logger_level
+    _g_evemon_default_logger_level = level
+
+
 def get_logger(tag: str) -> logging.Logger:
+    """
+    Create a logger with given name (tag).
+    :param tag: logger tag
+    :return: logging.Logger object named with tag.
+    """
+
     global _g_evemon_default_logger_level
     global _g_evemon_console_log_handler
     global _g_evemon_rotating_log_handler
@@ -49,6 +91,15 @@ def get_logger(tag: str) -> logging.Logger:
 
 
 def handle_unhandled_exception(exc_type, exc_value, exc_traceback):
+    """
+    This is a custom unhandled exceptions handler. Used internally, this function
+    is called as sys.excepthook. Calls any custom added unhandled exception handlers,
+    in the order of addition (see add_unhandled_exception_handler()).
+    :param exc_type: type of exception
+    :param exc_value: exception object thrown
+    :param exc_traceback: traceback object
+    :return: None
+    """
     global _g_evemon_unhandled_exception_logger
     # call original excepthook if we got here somehow with no logger created
     if _g_evemon_unhandled_exception_logger is None:
@@ -72,6 +123,14 @@ def handle_unhandled_exception(exc_type, exc_value, exc_traceback):
 
 
 def init_unhandled_exception_handling(log_filename: str):
+    """
+    You need to call this function before adding any unhandled exceptions handlers
+    (add_unhandled_exception_handler()). This overwrites sys.excepthook with custom
+    exception handler. This allows catching all uncaught exceptions and display them,
+    for example, in GUI message box popup.
+    :param log_filename: file name where to store unhandled exeptions
+    :return: None
+    """
     global _g_evemon_unhandled_exception_logger
     if _g_evemon_unhandled_exception_logger is None:
         # create and setup logger
@@ -92,6 +151,16 @@ def init_unhandled_exception_handling(log_filename: str):
 
 
 def add_unhandled_exception_handler(handler_func, bound_param):
+    """
+    Adds an unhandled exception handler, that will be called whenever
+    unhandled exception is caught. Handlers will be called in the order of addition.
+    Callback handler should have this prototype, almost the same as sys.excepthook:
+    handler_func(bound_param, exc_type, exc_value, exc_traceback)
+    bound_param will be passed as the first argument (may be None).
+    :param handler_func: callback handler function to call
+    :param bound_param: parameter that will be passed to callback handler function, may be None
+    :return: None
+    """
     global _g_evemon_unhandled_exception_handlers
     global _g_evemon_unhandled_exception_params
     if handler_func not in _g_evemon_unhandled_exception_handlers:
@@ -100,6 +169,11 @@ def add_unhandled_exception_handler(handler_func, bound_param):
 
 
 def remove_unhandled_exception_handler(handler_func):
+    """
+    Removes function handler_func from the list of unhandled exception handlers.
+    :param handler_func: handler callback function to remove
+    :return: None
+    """
     global _g_evemon_unhandled_exception_handlers
     global _g_evemon_unhandled_exception_params
     if handler_func in _g_evemon_unhandled_exception_handlers:
