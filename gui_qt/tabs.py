c312bc141226a04c07d4e3a2e8369f3514590b6d
Alexey Min <alexey.min@gmail.com>
2017-05-14 03:09:02 +0500

    Qt GUI: Overview tab "template"

--- gui_qt/tabs.py
+++ gui_qt/tabs.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel
 
 
 class AbstractBaseTab(QWidget):
@@ -16,3 +16,16 @@ class AbstractBaseTab(QWidget):
 
     def tab_type(self) -> str:
         return self._tab_type
+
+
+class OverviewTab(AbstractBaseTab):
+    def __init__(self, parent: QWidget = None):
+        super(OverviewTab, self).__init__(parent)
+        self._tab_type = AbstractBaseTab.TAB_TYPE_OVERVIEW
+        self._layout = QGridLayout()
+        self._layout.setContentsMargins(2, 2, 2, 2)
+        self._layout.setSpacing(3)
+        self.setLayout(self._layout)
+        #
+        self.lbl = QLabel('Overview tab', self)
+        self._layout.addWidget(self.lbl, 0, 0)
