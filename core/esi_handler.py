e822710afb9581a114b074eba978a7f871ba57ea
Alexey Min <alexey.min@gmail.com>
2017-05-20 17:42:14 +0500

    ESI: add documentation and method to return a proper query string
    to authorize in EVE-SSO OAuth2 service/

--- core/esi_handler.py
+++ core/esi_handler.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import base64
 from http import HTTPStatus
 import http.server
 import socketserver
 import threading
+import time
+import urllib.parse
 
 import core.logger
 import version
@@ -33,6 +36,12 @@ class EsiHttpRequestHandler(http.server.BaseHTTPRequestHandler):
         super(EsiHttpRequestHandler, self).handle()
 
     def do_GET(self):
+        """
+        This handles HTTP GET request.
+        When called from EVE-SSO it should receive query-string like this:
+        ?code=x9Y6F7K9Ij9KjRF0fxAVxJVZcLSiwkqfAq13aHIOyuyWoUbyN3jzP3_ZY1y3SchB0&state=MTQ5NTI3NzY5OC4yNjk3NTA0
+        :return: None
+        """
         content = '<h1>Hello!</h1>'
         content_utf8 = content.encode('utf-8', errors='ignore')
         #
@@ -81,12 +90,45 @@ class EsiHandlerThread(threading.Thread):
     def stop(self):
         # Ask TCPServer to stop select() loop on socket
         # and wait for it to shut down
+        # Does it work...?
         self._logger.debug('Stopping ESI handler server...')
         self.httpserver.shutdown()
         self._logger.debug('Stopped ESI handler.')
 
 
 def esi_handler_start() -> EsiHandlerThread:
+    """
+    Client code should only use this function to start
+    http server that receives EVE Online SSO Oauth2 reply.
+    :return: EsiHandlerThread instance, to probably call stop() on it
+    """
     thr = EsiHandlerThread()
     thr.start()
     return thr
+
+
+def esi_get_oauth_request_parameters() -> str:
+    ret = dict()
+    ret['response_type'] = 'code'
+    ret['redirect_uri'] = 'http://localhost:10233/'
+    ret['client_id'] = 'a67b73af98c64158a158a85d850b01a3'
+    ret['scope'] = 'publicData esi-calendar.read_calendar_events.v1 ' \
+                   'esi-location.read_location.v1 esi-location.read_ship_type.v1 ' \
+                   'esi-mail.read_mail.v1 esi-skills.read_skills.v1 ' \
+                   'esi-skills.read_skillqueue.v1 esi-wallet.read_character_wallet.v1 ' \
+                   'esi-search.search_structures.v1 esi-clones.read_clones.v1 ' \
+                   'esi-characters.read_contacts.v1 esi-universe.read_structures.v1 ' \
+                   'esi-bookmarks.read_character_bookmarks.v1 ' \
+                   'esi-killmails.read_killmails.v1 ' \
+                   'esi-corporations.read_corporation_membership.v1 ' \
+                   'esi-assets.read_assets.v1 esi-planets.manage_planets.v1 ' \
+                   'esi-fleets.read_fleet.v1 esi-fittings.read_fittings.v1 ' \
+                   'esi-markets.structure_markets.v1 esi-characters.read_loyalty.v1 ' \
+                   'esi-characters.read_opportunities.v1 ' \
+                   'esi-characters.read_chat_channels.v1 esi-characters.read_medals.v1 ' \
+                   'esi-characters.read_standings.v1 esi-characters.read_agents_research.v1 ' \
+                   'esi-industry.read_character_jobs.v1 esi-markets.read_character_orders.v1 ' \
+                   'esi-characters.read_blueprints.v1 esi-characters.read_corporation_roles.v1'
+    ret['state'] = base64.urlsafe_b64encode(str(time.time()).encode())
+    sss = urllib.parse.urlencode(ret)
+    return sss
